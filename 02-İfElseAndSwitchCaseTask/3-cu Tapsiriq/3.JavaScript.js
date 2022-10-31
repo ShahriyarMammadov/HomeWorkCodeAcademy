5f972c6ae65817bce7f91e60b95c40500ad13a72
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2022-10-31 15:10:57 +0400

    HomeWork

--- 02-İfElseAndSwitchCaseTask/3-cu Tapsiriq/3.JavaScript.js
+++ 02-İfElseAndSwitchCaseTask/3-cu Tapsiriq/3.JavaScript.js
@@ -4,25 +4,25 @@ Artan sıra: 1, 2, 3
 Azalan sıra: 3, 2, 1 */
 
 let a = 116
-let b = 116
-let c = 781
+let b = 786
+let c = 120
 
-if (a => b && a > c && b > c) {
+if (a > b && a > c && b > c) {
     console.log("Artan sıra: " + c + "," + b + "," + a)
     console.log("Azalan sıra: " + a + "," + b + "," + c)
-} else if (a => b && a > c && c > b) {
+} else if (a > b && a > c && c > b) {
     console.log("Artan sıra: " + b + "," + c + "," + a)
     console.log("Azalan sıra: " + a + "," + c + "," + b)
-} else if (b => a && b > c && c > a) {
+} else if (b > a && b > c && c > a) {
     console.log("Artan sıra: " + b + "," + c + "," + a)
     console.log("Azalan sıra: " + a + "," + c + "," + b)
-} else if (b => a && b > c && a > c) {
+} else if (b > a && b > c && a > c) {
     console.log("Artan sıra: " + c + "," + a + "," + b)
     console.log("Azalan sıra: " + b + "," + a + "," + c)
-} else if (c => a && c > b && b > a) {
+} else if (c > a && c > b && b > a) {
     console.log("Artan sıra: " + a + "," + b + "," + c)
     console.log("Azalan sıra: " + c + "," + b + "," + a)
-} else if (c => a && c > b && a > b) {
+} else if (c > a && c > b && a > b) {
     console.log("Artan sıra: " + b + "," + a + "," + c)
     console.log("Azalan sıra: " + c + "," + a + "," + b)
 }
